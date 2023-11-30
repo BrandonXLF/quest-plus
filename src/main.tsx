8326333bedb25c9612e1bd39724855f8ecb1f40f
Brandon Fowler <brandfowl@gmail.com>
2023-11-30 02:33:51 -0500

    Make td containing timetable full width

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,15 @@ function onPageLoaded() {
 
 	document.getElementById('wisp-timetable-root')?.remove();
 
+	const cnt = document.querySelector('[id*="STDNT_ENRL_SSV2"], [id*="DERIVED_REGFRM1_GROUP6"]');
+	
+	if (!cnt) return;
+
 	const root = document.createElement('div');
 	root.id = 'wisp-timetable-root';
-	document.querySelector('[id*="STDNT_ENRL_SSV2"], [id*="DERIVED_REGFRM1_GROUP6"]')?.prepend(root);
+
+	cnt?.prepend(root);
+	cnt.closest('td')!.style.width = '100%';
 
 	ReactDOM.createRoot(root).render(
 		<React.StrictMode>
