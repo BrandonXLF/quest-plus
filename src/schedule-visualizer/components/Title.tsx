b0e2a3e5f727ebbfff0bcf4e7ee63e94bd670667
Brandon Fowler <brandfowl@gmail.com>
2024-02-01 19:54:01 -0500

    Show Quest+ Schedule Planner in UI

--- src/schedule-visualizer/components/Title.tsx
+++ src/schedule-visualizer/components/Title.tsx
@@ -2,11 +2,11 @@ import './Title.css';
 
 export default function Title() {
 	const src = chrome.runtime.getURL('assets/logo.png');
-
+	
 	return (
 		<hgroup className="top-area">
 			<img src={src} />
-			<h2>WISP</h2>
+			<h2>Quest+ Schedule Planner</h2>
 		</hgroup>
 	);
 }
