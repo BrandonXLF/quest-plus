ec67785d8d481786a9af2607fde8912f8302202f
Brandon Fowler <brandfowl@gmail.com>
2026-07-28 00:47:28 -0400

    Show schedule everywhere

--- src/inject.ts
+++ src/inject.ts
@@ -14,3 +14,11 @@
 		window.dispatchEvent(new Event('quest-plus-page-nav'));
 	};
 }
+
+{
+	const scheduleToggle = document.querySelector<HTMLAnchorElement>('a[id*="DERIVED_REGFRM1_GROUP6"]');
+
+	if (scheduleToggle?.getAttribute('aria-expanded') === 'false') {
+		scheduleToggle.click();
+	}
+}
--- src/schedule-visualizer/data/getQuestParser.ts
+++ src/schedule-visualizer/data/getQuestParser.ts
@@ -4,5 +4,5 @@ import ScheduleParser from './ScheduleParser';
 
 export default function getQuestParser(): QuestParser | undefined {
 	if (document.getElementById('SSR_SSENRL_LIST')) return new ScheduleParser();
-	if (document.getElementById('SSR_SSENRL_CART')) return new CartParser();
+	if (document.querySelector('[id*="DERIVED_REGFRM1_GROUP6"]')) return new CartParser();
 }
